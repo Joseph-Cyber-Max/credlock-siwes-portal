55fc332e5508346520dcc1b6ec22ec98fcc020e0
Joseph <116102439+Joseph-Cyber-Max@users.noreply.github.com>
2026-09-09 12:04:22 +0100

    fix: centralize production portal URLs

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyTj3mbqgJA9ySkyr1gDUxS5tYKQs_r5hbMG53ol36pjjSkXBEUxjlG226woRADwUR-qg/exec';
+export const SHEET_ID = '1zGxiKWpJyerrCzn1vlTXBL9SMYJaxFy-6EtYCdvCL5w';
+export const SHEET_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/edit`;
+export const GITHUB_URL = 'https://github.com/Joseph-Cyber-Max/credlock-siwes-portal';
+export const PAGES_URL = 'https://joseph-cyber-max.github.io/credlock-siwes-portal/';
